411106ebc9dbd3f810c351b7a3fd1615a19f0592
mike.gaffney <mike.gaffney@mgaffney-l2.asynchrony.lan>
2008-10-05 18:31:43 -0500

    added in a test page for the feeds by xpath

--- app/controllers/admin/feeds_controller.rb
+++ app/controllers/admin/feeds_controller.rb
@@ -1,4 +1,20 @@
 class Admin::FeedsController < Admin::AdminBaseController
 	active_scaffold :feed do |config|
 	end
+	
+	def show
+		@record = Feed.find(params[:id])
+		doc = REXML::Document.new(Net::HTTP.get_response(URI.parse(@record.url)).body)
+		
+		ids = []
+		authors = []
+		
+		doc.elements.each(@record.id_xpath){|e| ids << e.text}
+		doc.elements.each(@record.author_xpath){|a| authors << a.text}
+		
+		@values = []
+		for i in 0..(ids.size - 1)
+			@values[i] = {:id => ids[i], :author => authors[i]}
+		end
+	end
 end
