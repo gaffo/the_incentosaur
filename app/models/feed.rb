3f2cdf5ea3bd18cb901aea18ecc1e2c8d7c56662
mike.gaffney <mike.gaffney@mgaffney-l2.asynchrony.lan>
2008-10-09 21:17:40 -0500

    feeds pull into db now

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,20 +4,24 @@ class Feed < ActiveRecord::Base
 	
 	def self.load_all_feeds
 		Feed.find(:all).each do |f|
-		 	f.load_current_from_feed	
+			begin
+		 		f.load_current_from_feed	
+	 		rescue => e
+	 			puts e
+ 			end
 		end
 	end
 	
 	def load_current_from_feed
 		pull_current_from_feed.each do |p|
-			next unless p[:author]
+			next if p[:author].empty?
 			author = AuthorFeed.find_or_create_by_feed_id_and_name(self.id, p[:author])
 			author.save!
 			post = Post.find_or_create_by_author_feed_idkey(
 							author,
 							self,
-							p[:idkey])
-			post.save!
+							p[:idkey],
+							p[:full_post])
 		end
 	end
 	
@@ -27,9 +31,10 @@ class Feed < ActiveRecord::Base
 		posts = []
 		
 		doc.elements.each(self.entry_xpath) do |entry|
-			post = Post.new(:idkey => entry.elements[self.id_xpath],
-							:author_feed => entry.elements[self.author_xpath],
-							:full_post => entry.to_s)
+			post = {:idkey => text_or_empty(entry.elements[self.id_xpath]),
+			        :author => text_or_empty(entry.elements[self.author_xpath]),
+			        :full_post => entry.to_s
+		}
 			posts << post
 		end
 		posts
@@ -38,6 +43,11 @@ class Feed < ActiveRecord::Base
 	protected
 	def pull_feed
 		REXML::Document.new(Net::HTTP.get_response(URI.parse(self.url)).body)
+	end
+	
+	def text_or_empty(element)
+		return element.text if element
+		""
 	end
 	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,15 @@ class Post < ActiveRecord::Base
 	belongs_to :author_feed
 	named_scope :recent, { :conditions => ["created_at > ?", Date.today - 7.days]}
 	
-	def self.find_or_create_by_author_feed_idkey(author, feed, idkey)
+	def self.find_or_create_by_author_feed_idkey(author, feed, idkey, full_post)
 		record = Post.exists?(:author_feed_id => author.id,
 							  :feed_id => feed.id,
 							  :idkey => idkey)
 		return record if record
-		Post.create(:author_feed => author,
-				  	:feed => feed,
-				  	:idkey => idkey)
+		post = Post.create(:author_feed => author,
+						  	:feed => feed,
+						  	:idkey => idkey,
+						  	:full_post => full_post)
+		return post
 	end
 end
