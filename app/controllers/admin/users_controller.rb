8d12f1c7b119bb06ae7b1aee162522181c9e23ea
gaffo <mike@uberu.com>
2008-10-23 21:32:33 -0500

    tweaked user admin ui

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::AdminBaseController
 	active_scaffold :user do |config|
+    config.list.columns = [:name, :is_admin, :author_feeds] 
 		config.columns.exclude :created_at, :updated_at
 		config.columns[:author_feeds].form_ui = :select
 	end
