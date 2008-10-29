be7a005d2289fd2eb09dc4ebd07bffbaa748414a
gaffo <mike@uberu.com>
2008-10-28 22:39:48 -0500

    [#20 state:closed] [#21 state:closed] froze gravatar, fixed user admin crud

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,8 @@
 class Admin::UsersController < Admin::AdminBaseController
 	active_scaffold :user do |config|
     config.list.columns = [:name, :is_admin, :author_feeds] 
+    config.create.columns = [:name, :email, :is_admin, :password]
+    config.update.columns = [:name, :email, :is_admin, :password]
 		config.columns.exclude :created_at, :updated_at
 		config.columns[:author_feeds].form_ui = :select
 	end
